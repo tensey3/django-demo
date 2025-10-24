4b9b3a48fb15d5149f499eda9b2100cb5f7f13b6
tensey3 <kikuchitensei428@gmail.com>
2025-10-24 16:35:15 +0900

    a

--- djangoprojects/blogproject/blogapp/urls.py
+++ djangoprojects/blogproject/blogapp/urls.py
@@ -1,9 +1,24 @@
+# from django.urls import path
+# from . import views
+
+# app_name = 'blogapp'
+
+# urlpatterns = [
+#     path("", views.IndexView.as_view(), name='index'),
+#     path("blog-detail/<int:pk>/", views.BlogDetailView.as_view(), name='blog_detail'),
+#     path("science/", views.ScienceListView.as_view(), name='science_list'),
+#     path("dailylife/", views.DailylifeListView.as_view(), name='dailylife_list'),
+#     path("music/", views.MusicListView.as_view(), name='music_list'),
+# ]
 from django.urls import path
 from . import views
 
-app_name = 'blogapp'
+app_name = "blogapp"
 
 urlpatterns = [
-    path("", views.IndexView.as_view(), name='index'),
-    path("blog-detail/<int:pk>/", views.BlogDetailView.as_view(), name='blog_detail'),
+    path("", views.IndexView.as_view(), name="index"),
+    path("blog-detail/<int:pk>/", views.BlogDetail.as_view(), name="blog_detail"),
+    path("science-list/", views.ScienceView.as_view(), name="science_list"),
+    path("dailylife-list/", views.DailylifeView.as_view(), name="dailylife_list"),
+    path("music-list/", views.MusicView.as_view(), name="music_list"),
 ]
--- djangoprojects/blogproject/blogapp/views.py
+++ djangoprojects/blogproject/blogapp/views.py
@@ -1,16 +1,69 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+# from django.views.generic import ListView, DetailView
+# from .models import BlogPost
+
+# class IndexView(ListView):
+#     """トップページのビュー"""
+#     template_name = "index.html"
+#     context_object_name = "orderby_records"
+#     queryset = BlogPost.objects.order_by("-posted_at")
+
+# class BlogDetailView(DetailView):
+#     """詳細ページのビュー"""
+#     template_name = "post.html"
+#     model = BlogPost
+
+# class ScienceListView(ListView):
+#     model = BlogPost
+#     template_name = 'blogapp/science_list.html'
+#     context_object_name = 'blog_list'
+
+#     def get_queryset(self):
+#         return BlogPost.objects.filter(category='science').order_by('-posted_at')
+
+
+# class DailylifeListView(ListView):
+#     model = BlogPost
+#     template_name = 'blogapp/dailylife_list.html'
+#     context_object_name = 'blog_list'
+
+#     def get_queryset(self):
+#         return BlogPost.objects.filter(category='dailylife').order_by('-posted_at')
+
+
+# class MusicListView(ListView):
+#     model = BlogPost
+#     template_name = 'blogapp/music_list.html'
+#     context_object_name = 'blog_list'
+
+#     def get_queryset(self):
+#         return BlogPost.objects.filter(category='music').order_by('-posted_at')
+from django.views.generic import ListView, DetailView
 from .models import BlogPost
 
 class IndexView(ListView):
-    """トップページのビュー
-    """
     template_name = "index.html"
     context_object_name = "orderby_records"
     queryset = BlogPost.objects.order_by("-posted_at")
+    paginate_by = 4
 
-class BlogDetailView(ListView):
-    """詳細ページのビュー
-    """
+class BlogDetail(DetailView):
     template_name = "post.html"
     model = BlogPost
+
+class ScienceView(ListView):
+    template_name = "science_list.html"
+    context_object_name = "science_records"
+    queryset = BlogPost.objects.filter(category='science').order_by("-posted_at")
+    paginate_by = 2
+
+class DailylifeView(ListView):
+    template_name = "dailylife_list.html"
+    context_object_name = "dailylife_records"
+    queryset = BlogPost.objects.filter(category='dailylife').order_by("-posted_at")
+    paginate_by = 2
+
+class MusicView(ListView):
+    template_name = "music_list.html"
+    context_object_name = "music_records"
+    queryset = BlogPost.objects.filter(category='music').order_by("-posted_at")
+    paginate_by = 2
